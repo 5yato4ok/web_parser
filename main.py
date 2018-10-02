660397739467da46be09d78ca26713237be4ddba
batman <pyatochok37@gmail.com>
2018-10-02 18:05:52 +0300

    Fixed getting sub-sub categories

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import xml.etree.ElementTree
 import requests
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, NavigableString
 
 class Gipfel_Parser:
     root_url = ''
@@ -13,22 +13,37 @@ class Gipfel_Parser:
         self.catalog_url = catalog_url_
 
     def parse_catalog(self):
-        soup = BeautifulSoup()
         par = {'p': 0}
         r = requests.get(self.catalog_url, params=par)
         soup = BeautifulSoup(r.text, 'html.parser')
-        categories_url = self.get_category_url()
-        for url in categories_url:
-            self.parse_category(self,url)
+        #self.categories_url = self.get_categories_url(soup)
+        self.sub_category_url = self.get_subcategory_url(soup)
+        #self.sub_category_url = self.get_subcategory_url()
+        #for url in categories_url:
+        #    self.parse_category(url)
+    def get_subcategory_url(self,soup):
+        subcategories_pages = soup.find_all('ul', {"class": "inner-menu"})
+        for div in subcategories_pages:
+            sub_sub = div.find_all('ul',{"class":"submenu"})
+            if sub_sub:
+                for sub_val in sub_sub:
+                    self.sub_category_url.update(self.get_inner_url(sub_val, self.root_url))
+            else: #TODO: add null item sub_sub
+                self.sub_category_url.update(self.get_inner_url(div, self.root_url))
+        return self.sub_category_url
 
+    def get_inner_url(self,div,add = ''):
+        test = set()
+        test.add(str(add + div.a['href']))
+        for value in div.contents:
+            if isinstance(value, NavigableString):
+                continue
+            test.add(str(add+ value.a['href']))
+        return test
     def parse_category(self, category_url):
-        soup = BeautifulSoup()
-        par = {'p': 0}
-        r = requests.get(category_url, params=par)
-        soup = BeautifulSoup(r.text, 'html.parser')
-        sub_cat_url = self.get_subcategory_url(self, category_url)
+        sub_cat_url = self.get_subcategory_url(category_url)
         for url in sub_cat_url:
-            self.parse_subcategory(self,url)
+            self.parse_subcategory(url)
 
     def parse_subcategory(self,subcat_url):
         par = {'p': 0}
@@ -42,8 +57,11 @@ class Gipfel_Parser:
     def parse_page(self, page_url):
         self.get_items_url(page_url)
 
-    def get_categories_url(self):
-        smth =2
+    def get_categories_url(self,soup):
+        categories_pages = soup.find_all('li', {"class": "menu_top_catalog_li"})
+        for div in categories_pages:
+            self.category_url.add(self.get_inner_url(div,self.root_url))
+        return self.category_url
 
     def get_pages_url(self,soup):
         div_pages = soup.find_all('div', {"class": "adm-navigation"})
@@ -51,9 +69,6 @@ class Gipfel_Parser:
         for div in div_pages:
             pages_url.add(self.get_inner_url(div,self.root_url))
         return pages_url
-
-    def get_subcategory_url(self):
-        smth =2
 
     def get_items_url(self,page_url):
         par = {'p': 0}
@@ -63,9 +78,8 @@ class Gipfel_Parser:
         for item in curp_items :
             self.items_url.add(self.get_inner_url(item,self.root_url)) #TODO: check non unique values
 
-    def get_inner_url(self,div,add = ''):
-        test = div.a['href']
-        return str(add + div.a['href'])
+
+
 
 
 
@@ -96,4 +110,5 @@ def parse_catolog(url):
 if __name__ == "__main__":
     #parse_catolog("https://gipfel.ru/catalog/nabory-posudy/nabory-kastryul-i-kovshey/")
     test_class = Gipfel_Parser('https://gipfel.ru','https://gipfel.ru/catalog')
-    test_class.parse_subcategory("https://gipfel.ru/catalog/nabory-posudy/nabory-kastryul-i-kovshey/")
+    #test_class.parse_subcategory("https://gipfel.ru/catalog/nabory-posudy/nabory-kastryul-i-kovshey/")
+    test_class.parse_catalog()
