0f4272859c94e6cf55eacb5310e4c2f4828d4d40
batman <pyatochok37@gmail.com>
2018-10-07 10:48:53 +0300

    Fixed getting url for a new design

--- main.py
+++ main.py
@@ -16,31 +16,35 @@ class Gipfel_Parser:
         par = {'p': 0}
         r = requests.get(self.catalog_url, params=par)
         soup = BeautifulSoup(r.text, 'html.parser')
-        #self.categories_url = self.get_categories_url(soup)
         self.sub_category_url = self.get_subcategory_url(soup)
-        #self.sub_category_url = self.get_subcategory_url()
-        #for url in categories_url:
-        #    self.parse_category(url)
+        for url in self.sub_category_url:
+            self.parse_subcategory(url)
+
     def get_subcategory_url(self,soup):
-        subcategories_pages = soup.find_all('ul', {"class": "inner-menu"})
-        for div in subcategories_pages:
-            without_child = div.find_all('li',{"class":" "})
-            sub_sub = div.find_all('ul', {"class": "submenu"})
-            for child_free in without_child:
-                self.sub_category_url.update(self.get_inner_url(child_free, self.root_url,True))
-            for sub_val in sub_sub:
-                self.sub_category_url.update(self.get_inner_url(sub_val, self.root_url))
+        parent_blocks = soup.find_all('div', {"class": "submenu-block"})
+        for div in parent_blocks:
+            heads = div.find_all('div',{"class":"submenu-item submenu-item--more"})
+            add_head = div.find_all('div',{"class":"submenu-item "})
+            for value in add_head:
+                heads.insert(len(heads),value)
+            for head in heads:
+                children = head.find_all('div', {"class": "submenu-item-body"})
+                for child in children:
+                   self.sub_category_url.update(self.get_inner_url(child,self.root_url))
+                if not children:
+                    self.sub_category_url.update(self.get_inner_url(head,self.root_url))
+
         return self.sub_category_url
 
-    def get_inner_url(self,div,add = '',check_root = False):
+    def get_inner_url(self,div,add = ''):
         test = set()
         test.add(str(add + div.a['href']))
-        if not check_root:
-            for value in div.contents:
-                if isinstance(value, NavigableString): # or not value.has_attr('href')
-                    continue
-                test.add(str(add+ value.a['href']))
+        links = div.find_all('a href')
+        for a in links:
+            test.add(str(add + a['href']))
         return test
+
+
     def parse_category(self, category_url):
         sub_cat_url = self.get_subcategory_url(category_url)
         for url in sub_cat_url:
@@ -53,6 +57,7 @@ class Gipfel_Parser:
         pages_url = self.get_pages_url(soup)
         pages_url.add(subcat_url)
         for page in pages_url:
+            #need to check for additional pages
             self.parse_page(page)
 
     def parse_page(self, page_url):
@@ -65,48 +70,20 @@ class Gipfel_Parser:
         return self.category_url
 
     def get_pages_url(self,soup):
-        div_pages = soup.find_all('div', {"class": "adm-navigation"})
+        div_pages = soup.find_all('div', {"class": "module-pagination"})
         pages_url  = set()
         for div in div_pages:
-            pages_url.add(self.get_inner_url(div,self.root_url))
+            pages_url.update(self.get_inner_url(div,self.root_url))
         return pages_url
 
     def get_items_url(self,page_url):
         par = {'p': 0}
         r = requests.get(page_url, params=par)
         soup = BeautifulSoup(r.text, 'html.parser')
-        curp_items = soup.find_all('div', {"class": "item_info TYPE_1"})
+        curp_items = soup.find_all('div', {"class": "main__goods--item main__goods--tile"})
         for item in curp_items :
-            self.items_url.add(self.get_inner_url(item,self.root_url)) #TODO: check non unique values
+            self.items_url.update(self.get_inner_url(item,self.root_url)) #TODO: check non unique values
 
-
-
-
-
-
-def get_items_url2(category_page_url,soup):
-    curp_items = soup.find_all('div', {"class": "item_info TYPE_1"})
-    result = get_inner_url(curp_items, 'https://gipfel.ru')
-    return result
-
-
-def get_inner_url(productDivs, add='https://gipfel.ru'):
-    result = []
-    for div in productDivs:
-        result.append(str(add + div.a['href']))
-    return result
-
-
-def parse_catolog(url):
-    par = {'p': 0}
-    r = requests.get(url, params=par)
-    soup = BeautifulSoup(r.text, 'html.parser')
-    div_pages = soup.find_all('div', {"class": "adm-navigation"})
-    add_url_pages = get_inner_url(div_pages,'https://gipfel.ru')
-    add_url_pages.append(url)
-    items_url = []
-    for url in add_url_pages:
-        items_url+=get_items_url(url,soup)
 
 if __name__ == "__main__":
     #parse_catolog("https://gipfel.ru/catalog/nabory-posudy/nabory-kastryul-i-kovshey/")
